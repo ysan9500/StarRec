17f757165b7fae74c9373d11240dd779a324f59e
ysan9500 <ysan9500@gmail.com>
2024-07-16 17:24:18 +0900

    Fixed load.py to bypass request blocking.

--- load.py
+++ load.py
@@ -4,6 +4,10 @@ import os
 from langchain_community.document_loaders import WebBaseLoader
 
 import nest_asyncio
+headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+           'Accept-Encoding': 'gzip, deflate',
+           'Accept-Language': 'en-US,en;q=0.9',
+           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}
 
 
 def load_news():
@@ -14,16 +18,26 @@ def load_news():
     docs = []
     
     for keyword in keywords:
-        limit = 6
         links = news_dict[keyword]['link'].to_list()
-        for link in links:
-            if limit <= 0: break
-            loader = WebBaseLoader(link)
-            doc = loader.load()
-            docs.append(doc)
-            print(link)
-            limit -= 1
-            #print(doc)
+
+#        for link in links:
+#            if limit <= 0: break
+#            loader = WebBaseLoader(link)
+#            loader.requests_per_second = 1
+#            doc = loader.load()
+#            docs.append(doc)
+#            print(link)
+#            limit -= 1
+#
+#            #print(doc)
+
+        #TODO: Handling exceptions for requests taking too long.
+
+        loader = WebBaseLoader(links, header_template=headers, verify_ssl=True, continue_on_failure=True, bs_get_text_kwargs=)
+        loader.requests_per_second = 10
+        doc = loader.aload()
+        docs.append(doc)
+        print(links)
     return docs
 
 def load_preference(num):
@@ -44,4 +58,4 @@ def load_preference(num):
 
 
 if __name__ == "__main__":
-    load_preference(3)
+    load_news()
